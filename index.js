0e4b28774692da2eb603b2cd3c6e5d001c785ce0
Bappi Das <151029855+bappidass@users.noreply.github.com>
2024-05-03 09:57:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,22 @@ function strutus(){
   return staus='success';
 }
 
+
 withdraw_click.addEventListener('click',()=>{
     let user_amount=Number(document.querySelector('.user-amount').value);
     if(user_amount==''){
-      document.querySelector('.user-problem').innerHTML+=`<p style="color:red;">Enter a vaild amount..</p>`
+      document.querySelector('.user-problem').innerHTML=`<p style="color:red;">Enter a vaild amount..</p>`
       user_amount='';
      setTimeout(changevalue,2000);
     //  changevalue();
     }else if(user_amount<100){
-      document.querySelector('.withdraw-input-amount').innerHTML+=`<p style="color:red;">Enter amount more then 100</p>`
+      document.querySelector('.user-problem').innerHTML=`<p style="color:red;">Enter amount more then 100</p>`
       user_amount='';
+      setTimeout(changevalue,2000);
     }else if(user_amount>withdrable.innerHTML){
-      document.querySelector('.withdraw-input-amount').innerHTML+=`<p style="color:red;">insufficient balance</p>`
+      document.querySelector('.user-problem').innerHTML=`<p style="color:red;">insufficient balance</p>`
       user_amount='';
+      setTimeout(changevalue,2000);
     }else{
       withdrable.innerHTML-=user_amount;
       
